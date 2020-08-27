b806f668c69cd610e4d4cacffb441db8edc8c6e2
ysherqawi <ysherqawi@gmail.com>
2020-08-27 13:31:46 +0300

    setup routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Join from './components/Join';
+import Chat from './components/Chat';
 
-const App = () => <>Welcome</>;
+const App = () => (
+  <>
+    <Router>
+      <Switch>
+        <Route path='/chat' component={Chat} />
+        <Route path='/' exact component={Join} />
+      </Switch>
+    </Router>
+  </>
+);
 
 export default App;
