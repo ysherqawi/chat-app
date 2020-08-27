56203df76efdff0c9e03ff5a6af0eb602c6f0eaf
ysherqawi <ysherqawi@gmail.com>
2020-08-27 16:20:02 +0300

    setup user joining and disconnecting

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ const io = socketio(server);
 io.on('connection', (socket) => {
   console.log('We have a new connection!!');
 
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+  });
   socket.on('disconnect', () => {
     console.log('User has left!!');
   });
