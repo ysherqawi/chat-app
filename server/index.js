0da013ba6a819eccd645747215f0c49c494681e8
ysherqawi <ysherqawi@gmail.com>
2020-08-27 14:09:53 +0300

    setup server and socketio

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log('We have a new connection!!');
+
+  socket.on('disconnect', () => {
+    console.log('User has left!!');
+  });
+});
+
+const router = require('./router');
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
