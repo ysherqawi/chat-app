ba21b2be5c554961de99db2afbec0a637fa1dea4
ysherqawi <ysherqawi@gmail.com>
2020-08-27 20:03:17 +0300

    completed user join functionality

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -10,8 +12,24 @@ io.on('connection', (socket) => {
   console.log('We have a new connection!!');
 
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { user, error } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name}, has joined !` });
+
+    socket.join(user.room);
+
+    callback();
   });
+
   socket.on('disconnect', () => {
     console.log('User has left!!');
   });
